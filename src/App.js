01b1f5ccdb7f0d939a47be9acf4a76179374af59
Ryan Cameron <camerorb@grace.edu>
2017-05-30 16:29:31 -0400

    add github

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { NavLink, Switch, Route } from 'react-router-dom'
 import './App.css';
+import Github from './Github'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <div className="App-headings">
+            <h3>Ain't no party like an</h3>
+            <h1>API Party</h1>
+          </div>
+          <ul className="nav-links">
+            <li>
+              <NavLink to='/github'>Github API</NavLink>
+            </li>
+            <li>
+              <NavLink to='/nasa'>NASA API</NavLink>
+            </li>
+            <li>
+              <NavLink to='/homework'>Homework</NavLink>
+            </li>
+          </ul>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Switch>
+          <Route path='/github' component={Github} />
+          <Route render={() => <p>To get started, click one of the links above</p>} />
+        </Switch>
       </div>
     );
   }
 }
-
 export default App;
